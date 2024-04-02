665cedb97f31a802da78a153bef3d5f9876a89f3
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-04-02 14:52:21 +0530

    add frontend files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 
 // Importing pages and components
-import Review from './pages/review_form';
-import Header from './components/header';
+//  import Review from './pages/review_form';
+import Header_dark from './components/header_simple_dark';
 
 function App() {
   return (
     <>
     <div className="App">
       <BrowserRouter>
-       <Header /> 
+       <Header_dark /> 
         <div className="pages">
           <Routes>
             <Route
