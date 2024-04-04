afc7781f7f58dfe17cb46c644769b9113e5fb3f9
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-04-04 10:44:18 +0530

    Add repair controller

--- server/routes/repair.js
+++ server/routes/repair.js
@@ -1,38 +1,25 @@
 const express = require('express');
-const Repair = require('../models/repair_model');
+// const Repair = require('../models/repair_model');
+const {createRepair,
+        get_repairs,
+        get_repair,
+        deleteRepair,
+        updateRepair} = require('../controllers/repairs_controller');
 const router = express.Router();
 
 //get all repairs
-router.get('/', (req, res) => {
-    res.json({mssg: "Get all repairs!"})
-});
-
+router.get('/', get_repairs);
+    
 //get a single repair
-router.get('/:id', (req, res) => {
-    res.json({mssg: "Get a single repair!"})
-});
+router.get('/:id', get_repair);
 
 //post a new repair
-router.post('/', async(req, res) => {
-    const {name, email, date, device_brand, device_model, problem, description} = req.body;
-    try {
-        const repair = await Repair.create({name, email, date, device_brand, device_model, problem, description})
-        res.status(200).json({repair})
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-
-    // res.json({mssg: "Post a new repair!"})
-});
+router.post('/', createRepair);
 
 //delete a repair
-router.delete('/:id', (req, res) => {
-    res.json({mssg: "Delete a repair!"})
-});
+router.delete('/:id', deleteRepair);
 
 //update the repair
-router.patch('/:id', (req, res) => {
-    res.json({mssg: "Update a repair!"})
-});
+router.patch('/:id', updateRepair);
 
 module.exports = router;  
