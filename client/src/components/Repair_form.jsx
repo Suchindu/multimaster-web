be7ac4f9b38c6bbc249938521eb5fa6985600baa
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-04-08 12:56:57 +0530

    Update Repair_form

--- client/src/components/Repair_form.jsx
+++ client/src/components/Repair_form.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+//component
+import RepairAlert_box from './RepairAlert_box';
 const Repair_form = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -9,12 +10,46 @@ const Repair_form = () => {
     const [device_model, setDeviceModel] = useState('');
     const [problem, setProblem] = useState('');
     const [description, setDescription] = useState('');
+    const [emailError, setEmailError] = useState(null);
+    const [contactError, setContactError] = useState(null);
     const [error, setError] = useState(null);
+    const [showAlert, setShowAlert] = useState(false);
 
-    // const handleChange = (e) => {
-    //     setFormData({ ...formData, [e.target.name]: e.target.value });
+        //email validation
+     const handleEmailChange = (e) => {
+            const input = e.target.value;
+            setEmail(input);
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regular expression
+            if(!emailPattern.test(input)){
+                setEmailError("Please enter a valid email address");
+        }else {    
+            setEmailError(null);
+        }
+    };
 
-    // };
+    //contact validation
+    const handleContactChange = (e) => {
+        const input = e.target.value; // Remove non-digit characters
+        if(!/^\d*$/.test(input)){
+            setContactError("Please enter a valid phone number");
+        }else {
+            setContact(input);
+            setContactError(null);
+        }
+
+    };
+
+    //pop up box timing
+    useEffect(() => {
+        //after 3 second hide the alert box
+        const timeout = setTimeout(() => {
+            setShowAlert(false);
+        },  2000)
+
+    return () => clearTimeout(timeout);
+}, [showAlert]);
+
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -44,7 +79,8 @@ const Repair_form = () => {
             setProblem('');
             setDescription('');
             setError(null);
-            alert("New Repair Request Added");
+            //alert("New Repair Request Added");
+            setShowAlert(true);
         }
     };
 
@@ -81,10 +117,11 @@ const Repair_form = () => {
                         id="email"
                         name="email"
                         required="required"
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={handleEmailChange}
                         value={email}
                         className="w-full md:w-64 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
+                    {emailError && <p className="text-red-800">{emailError}</p>}
                 </div> <br/>
 
                    <div>
@@ -96,11 +133,14 @@ const Repair_form = () => {
                         id="contact"
                         name="contact"
                         required="required"
+                        pattern="0[0-9]{9}"
+                        title="Please enter a 10-digit phone number starting with '0'"
                         maxLength={10}
-                        onChange={(e) => setContact(e.target.value)}
+                        onChange={handleContactChange}
                         value={contact}
-                        className="w-full md:w-64 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="w-full md:w-64 px-4 py-2 border borddser-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
+                    {contactError && <p className="text-red-800">{contactError}</p>}
                 </div> <br/>
 
                     <div>
@@ -184,10 +224,12 @@ const Repair_form = () => {
             >
                 Submit
             </button>
-            {error && <p className="text-red-800">{error}</p>}
+            {/* {error && <p className="text-red-800">{error}</p>} */}
         </form>
-        </>
-    );
+        {showAlert && <RepairAlert_box message="New Repair Request Added" />
+        }
+    </>
+    ); 
 };
 
 export default Repair_form;
