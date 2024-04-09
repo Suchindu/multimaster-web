2c137561a420b4d2af2434a59f5920043acea7d0
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-04-10 00:03:45 +0530

    Update Repair_details

--- client/src/components/Repair_details.jsx
+++ client/src/components/Repair_details.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 //import { BsFillPencilFill, BsFillTrashFill } from 'react-icons/bs';
 
-function Repair_details() {
-    // Your code here
-
+const Repair_details = ({repair}) => {
+    // console.log(repair);
     return (
         // JSX code for the component
         <>
-        <div>
-            <h1 className='text-center font-bold text-2xl'>Repair Requets</h1>
-        </div>
-        <div>
-            <div className="flex justify-center mt-4">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    onChange={handleFilter}
-                    className="px-4 w-9/12 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button className="ml-2 justify-center px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    Search
-                </button>
-                </div>
-            </div>
-        <div className='mt-7 flex justify-center '>
-          
-        </div>
+                        {repair ?(
+                        <tr>
+                            <td className='border border-gray-500'>{repair.name}</td>
+                            <td className='border border-gray-500'>{repair.email}</td>
+                            <td className='border border-gray-500'>{repair.contact}</td>
+                            <td className='border border-gray-500'>{repair.date}</td>
+                            <td className='border border-gray-500'>{repair.device_brand}</td>
+                            <td className='border border-gray-500'>{repair.device_model}</td>
+                            <td className='border border-gray-500'>{repair.problem}</td>
+                            <td className='border border-gray-500'>{repair.description}</td>
+                            <td className='border border-gray-500 flex justify-center items-center'>
+                                <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mr-2">
+                                    Edit
+                                </button>
+                                <button className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                        ) : (
+                            <tr>
+                                <td colSpan={9}>No repair requests</td>
+                            </tr>
+                        )}
         </>
         
     );
