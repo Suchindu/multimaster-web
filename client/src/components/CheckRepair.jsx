f416123c7499ce42f500aa79e8115ca200e62626
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-04-13 18:36:50 +0530

    Implement check repair

--- client/src/components/CheckRepair.jsx
+++ client/src/components/CheckRepair.jsx
@@ -1,8 +1,31 @@
 // fetch details from the database and add the search bar to filter the data
-import React from 'react';
+import React, { useState } from 'react';
+//components
+import Header from './header';
+import Footer from './footer';
 
-function CheckRepair() {
-    
+const CheckRepair = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+        // Perform filtering logic here
+    };
+
+    return (
+        <>
+        <Header />
+        <div>
+            
+            <h1 className='mt-40 ml-60 text-2xl font-semibold flex-auto text-blue-800'>Check Repair</h1>
+            <input type="text" value={searchTerm} className='mt-2.5 ml-60 w-1/3  border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-800 shadow-lg' onChange={handleSearch} placeholder="Search..." />
+            {/* Render filtered data here */}
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, labore et, quam provident assumenda ea voluptates nisi minus aperiam consequuntur in esse ducimus voluptatem pariatur officiis doloremque vero molestias iste.</p>
+            
+        </div>
+        <Footer />
+        </>
+    );
 }
 
 export default CheckRepair;
