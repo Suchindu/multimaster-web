b1a909bcd6e86e5142e1be8d60d9e94dc7c6fabe
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-04-15 12:41:09 +0530

    Change App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Importing pages and components
 // import Review from './pages/review_form';
 // import Header from './components/header';
 // import Footer from './components/footer';
-import Repair_form from './components/Repair_form';
+import Repair_form from "./components/Repair_form";
+import Repair_table from "./pages/Repair_table";
+import CheckRepair from "./components/CheckRepair";
 
 // function App() {
 //   return (
 //     <>
 //     <div className="App">
 //       <BrowserRouter>
-//        <Header/> 
+//        <Header/>
 //         <div className="pages">
 //           <Routes>
 //             <Route
@@ -30,19 +32,18 @@ import Repair_form from './components/Repair_form';
 function App() {
   return (
     <>
-    <div className="App">
-      <BrowserRouter>
-        <div className="pages">
-          <Routes>
-            <Route
-              path='/'
-              element={<Repair_form />}
-            />
-          </Routes>
-        </div>
-        {/* <Footer/> */}
-      </BrowserRouter>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <div className="pages">
+            <Routes>
+              <Route path="/" element={<Repair_form />} />
+              <Route path="Repair_table" element={<Repair_table />} />
+              <Route path="Check_repair" element={<CheckRepair />} />
+            </Routes>
+          </div>
+          {/* <Footer/> */}
+        </BrowserRouter>
+      </div>
     </>
   );
 }
