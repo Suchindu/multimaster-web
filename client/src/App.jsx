ff0fc2e2df51939cea83d38749e0b37d7fe3f661
Heshan Kodithuwakku <it22589804@my.sliit.lk>
2024-04-17 15:13:51 +0530

    Auth and Checkout Updated.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 //Importing pages and components
-import Review from './pages/Reviews';
-import Header from './components/header';
-import Footer from './components/footer';
-import Repair_form from './components/Repair_form';
-
 import Cart from './components/Cart.jsx';
 import Home from './pages/Home.jsx';
 import CheckOut from './components/checkout.jsx';
+import Login from "./components/Auth/Login.jsx";
+import Register from "./components/Auth/Register.jsx";
+import UserProfile from "./components/Auth/UserProfile.jsx";
 
-
-
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="app">
-      <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route path='/cart' element={<Cart />}/>
-        <Route path='/checkout' element={<CheckOut />}/>
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="app">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<CheckOut />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- client/src/components/checkout.jsx
+++ client/src/components/checkout.jsx
@@ -1,62 +1,105 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import axios from "axios"; // Import Axios for making HTTP requests
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { v4 as uuidv4 } from "uuid";
+import { useQuery } from "@tanstack/react-query";
+import { emptyCart } from "../redux/cartSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function Checkout() {
   const cart = useSelector((state) => state.cart);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    mobileNumber: "",
-    country: "Sri Lanka",
-    streetAddress: "",
-    city: "",
-    region: "",
-    postalCode: "",
-    additionalDetails: "",
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const { data } = useQuery({
+    queryKey: ["getUser"],
+    cacheTime: 15 * (60 * 1000),
+    staleTime: 10 * (60 * 1000),
+    queryFn: async () => {
+      const token = localStorage.getItem("token");
+
+      if (token === null) {
+        throw new Error("Error retrieving user details");
+      }
+
+      try {
+        const response = await axios.post("http://localhost:4000/profile", {
+          token: token,
+        });
+        if (response.data.status === "ok") {
+          return response.data.user; // Update user state with received user data
+        } else {
+          console.error("Error retrieving user details:", response.data.data);
+
+          throw new Error("Error retrieving user details");
+        }
+      } catch (error) {
+        console.error("Error retrieving user details:", error.message);
+
+        throw new Error("Error retrieving user details");
+      }
+    },
   });
 
-  const handleFormChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  useEffect(() => {
+    if (data) {
+      setValue("name", data?.name ?? null);
+      setValue("email", data?.email ?? null);
+      setValue("address", data?.address ?? null);
+      setValue("phone", data?.phone ?? null);
+    }
+  }, [data]);
 
-  const handleFileChange = (e) => {
-    // Handle file upload if needed
-  };
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // Prevent default form submission
+  const onSubmit = async (data) => {
+    console.log(cart);
     try {
       // Combine form data and cart items
       const orderData = {
-        // Add the necessary fields from formData
-        firstName: formData.firstName,
-        lastName: formData.lastName,
-        email: formData.email,
-        mobileNumber: formData.mobileNumber,
-        streetAddress: formData.streetAddress,
-        city: formData.city,
-        province: formData.region, // Or change to province if that's what backend expects
-        postalCode: formData.postalCode,
-        // Add other fields as needed
-        // Ensure that the cart items are properly formatted
-        cart: cart.map(item => ({
-          productId: item.productId,
-          quantity: item.quantity,
-          // Add other product-related fields if needed
-        })),
+        uid: uuidv4(),
+        name: data.name,
+        email: data.email,
+        address: data.address,
+        phone: data.phone,
+        products: cart,
+        totalPrice: 0,
+        additionalDetails: data.additionalDetails,
       };
 
-      // Send POST request to backend to submit order with cart items
-      const response = await axios.post("http://localhost:5000/api/orders", orderData);
+      console.log(orderData);
 
-      // Handle successful response (e.g., show confirmation message)
+      // Send POST request to backend to submit order with cart items
+      const response = await axios.post(
+        "http://localhost:4000/api/orders",
+        orderData
+      );
+
       console.log("Order submitted:", response.data);
+
+      if (response.status === 200) {
+        alert("Success");
+
+        dispatch(emptyCart());
+
+        navigate("/");
+      } else {
+        alert("Error Submitting Data");
+      }
     } catch (error) {
       // Handle error
       console.error("Error submitting order:", error);
+
+      alert("Error Submitting Data");
     }
   };
 
@@ -66,19 +109,13 @@ export default function Checkout() {
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Checkout
         </h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12">
-
-
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-    
-
-               
-              </div>
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"></div>
             </div>
 
-            <div className="border-b border-gray-900/10 pb-12">
+            <div className="border-b border-gray-900/10 pb-6">
               <h2 className="text-base font-semibold leading-7 text-gray-900">
                 Delivery Details
               </h2>
@@ -87,43 +124,32 @@ export default function Checkout() {
               </p>
 
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
+                <div className="col-span-full">
                   <label
-                    htmlFor="first-name"
+                    htmlFor="name"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    First name
+                    Name
                   </label>
                   <div className="mt-2">
                     <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
+                      {...register("name", {
+                        required: true,
+                        maxLength: 200,
+                      })}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
+                    {errors.name ? (
+                      <p className="text-red-500 text-sm my-1 mx-3">
+                        *Required
+                      </p>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
 
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="last-name"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Last name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-4">
+                <div className="col-span-full">
                   <label
                     htmlFor="email"
                     className="block text-sm font-medium leading-6 text-gray-900"
@@ -132,131 +158,78 @@ export default function Checkout() {
                   </label>
                   <div className="mt-2">
                     <input
-                      id="email"
-                      name="email"
+                      {...register("email", {
+                        required: true,
+                        maxLength: 200,
+                      })}
                       type="email"
                       autoComplete="email"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
+                    {errors.email ? (
+                      <p className="text-red-500 text-sm my-1 mx-3">
+                        *Required
+                      </p>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
 
-
-                <div className="sm:col-span-3">
+                <div className="col-span-full">
                   <label
-                    htmlFor="first-name"
+                    htmlFor="number"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
                     Mobile Number
                   </label>
                   <div className="mt-2">
                     <input
-                      type="text"
-                      name="mobile-number"
-                      id="mobile-number"
-                      autoComplete="given-name"
+                      {...register("phone", {
+                        required: true,
+                        maxLength: 200,
+                      })}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="country"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Country
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      id="country"
-                      name="country"
-                      autoComplete="country-name"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                    >
-                      <option>Sri lanka</option>
-                    </select>
+                    {errors.phone ? (
+                      <p className="text-red-500 text-sm my-1 mx-3">
+                        *Required
+                      </p>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
 
                 <div className="col-span-full">
                   <label
-                    htmlFor="street-address"
+                    htmlFor="address"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Street address
+                    Address
                   </label>
                   <div className="mt-2">
-                    <input
-                      type="text"
-                      name="street-address"
-                      id="street-address"
-                      autoComplete="street-address"
+                    <textarea
+                      {...register("address", {
+                        required: true,
+                        maxLength: 1000,
+                      })}
+                      rows={3}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2 sm:col-start-1">
-                  <label
-                    htmlFor="city"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    City
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="city"
-                      id="city"
-                      autoComplete="address-level2"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="region"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    State / Province
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="region"
-                      id="region"
-                      autoComplete="address-level1"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="postal-code"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    ZIP / Postal code
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="postal-code"
-                      id="postal-code"
-                      autoComplete="postal-code"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
+                    {errors.address ? (
+                      <p className="text-red-500 text-sm my-1 mx-3">
+                        *Required
+                      </p>
+                    ) : (
+                      <></>
+                    )}
                   </div>
                 </div>
               </div>
             </div>
 
-       
-
             {/* Payment Slip Upload */}
-
 
             <div className="col-span-full">
               <label
@@ -293,36 +266,35 @@ export default function Checkout() {
               </div>
             </div>
 
+            {/* Addtional information */}
 
-                 {/* Addtional information */}
-
-                 <div className="col-span-full">
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Additional Details
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={""}
-                    />
-                  </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-600">
-                    Please add any additional information about the order that you think is important.
-                  </p>
-                </div>
-
-
-
-
-
-
-
+            <div className="col-span-full">
+              <label
+                htmlFor="about"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Additional Details
+              </label>
+              <div className="mt-2">
+                <textarea
+                  {...register("additionalDetails", {
+                    required: false,
+                    maxLength: 1000,
+                  })}
+                  rows={3}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.additionalDetails ? (
+                  <p className="text-red-500 text-sm my-1 mx-3">*Required</p>
+                ) : (
+                  <></>
+                )}
+              </div>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Please add any additional information about the order that you
+                think is important.
+              </p>
+            </div>
           </div>
 
           <div className="mt-6 flex items-center justify-end gap-x-6">
