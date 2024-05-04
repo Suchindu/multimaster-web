2c2bc5eab94b06654f8f6375d25dd6c89fc20c8c
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-05-04 18:12:57 +0530

    Add custom id

--- server/routes/repair.js
+++ server/routes/repair.js
@@ -1,25 +1,31 @@
-const express = require('express');
+const express = require("express");
 // const Repair = require('../models/repair_model');
-const {createRepair,
-        get_repairs,
-        get_repair,
-        deleteRepair,
-        updateRepair} = require('../controllers/repairs_controller');
+const {
+  createRepair,
+  get_repairs,
+  get_repair,
+  get_repair_by_str_id,
+  deleteRepair,
+  updateRepair,
+} = require("../controllers/repairs_controller");
 const router = express.Router();
 
 //get all repairs
-router.get('/', get_repairs);
-    
+router.get("/", get_repairs);
+
 //get a single repair
-router.get('/:id', get_repair);
+router.get("/:id", get_repair);
+
+//get a single repair by repair_id_str
+router.get("/str/:id", get_repair_by_str_id);
 
 //post a new repair
-router.post('/', createRepair);
+router.post("/", createRepair);
 
 //delete a repair
-router.delete('/:id', deleteRepair);
+router.delete("/:id", deleteRepair);
 
 //update the repair
-router.patch('/:id', updateRepair);
+router.patch("/:id", updateRepair);
 
-module.exports = router;  
+module.exports = router;
