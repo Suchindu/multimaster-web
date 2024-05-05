2bc9008690098175dc99cbdcd9d9d402e2390347
Suchindu Malith <122116912+Suchindu@users.noreply.github.com>
2024-05-06 04:41:42 +0530

    Header Update with Login Function
    Now the User can View profie and sign out from header

--- client/src/components/store/ProductGrid.jsx
+++ client/src/components/store/ProductGrid.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import Product from "./Product";
 import React,{ useState, useEffect} from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
 import { useQuery } from "@tanstack/react-query";
 
 
@@ -22,18 +21,6 @@ axios.get("http://localhost:4000/api/products")
     });
 
 }, []);
-
-const cart = useSelector((state) => state.cart);
-
-  const navigate = useNavigate();
-
-  const getTotalQuantity = () => {
-    let total = 0;
-    cart.forEach((item) => {
-      total += item.quantity;
-    });
-    return total;
-  };
 
   const { isSuccess } = useQuery({
     queryKey: ["getUser"],
@@ -67,28 +54,8 @@ const cart = useSelector((state) => state.cart);
     
 
     return (
-      <div>
       <div className="flex justify-between items-center my-4 mx-2">
-        {isSuccess ? (
-          <NavLink to={`/profile`}>
-            <div className="bg-red-500 py-2 px-4 rounded text-white font-bold cursor-pointer">
-              Profile
-            </div>
-          </NavLink>
-        ) : (
-          <NavLink to={`/login`}>
-            <div className="bg-blue-600 py-2 px-4 rounded text-white font-bold cursor-pointer">
-              Login
-            </div>
-          </NavLink>
-        )}
-
-        {/* <div className="ClockIcon" onClick={() => navigate("/cart")}>
-          <ClockIcon id="cartIcon" />
-          <p>{getTotalQuantity() || 0}</p>
-        </div> */}
-        
-      </div>
+      
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
         {products.map((product) => {
             return (
@@ -96,8 +63,8 @@ const cart = useSelector((state) => state.cart);
             )
         })}
         </div>
-      
-    </div>
+      </div>
+    
     )
 
 }
