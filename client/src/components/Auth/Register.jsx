a67ef39ba4188b8c928076b06f313c91615f2ff2
Suchindu Malith <122116912+Suchindu@users.noreply.github.com>
2024-05-06 02:58:05 +0530

    User Profile Features are Updated

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -48,101 +48,253 @@ function Register() {
   };
 
   return (
-    <div>
-      <h1 className="login-topic">Join With Us..!</h1>
+  //   <div>
+  //     <h1 className="login-topic">Join With Us..!</h1>
 
-      <div className="user_tabl_towcolum">
-        <div className="left_colum_user">
-          <img src={regiimg} alt="regi img" className="regi_img" />
-        </div>
-        <div className="riight_colum_user">
-          <form className="regi-form" onSubmit={handleSubmit}>
-            <label className="login-lable">Full Name</label>
-            <br />
-            <input
-              type="text"
-              name="name"
-              value={user.name}
-              onChange={handleInputChange}
-              className="login-input"
-              required
-            />
-            <br />
-            <label className="login-lable">Email</label>
-            <br />
-            <input
-              type="email"
-              value={user.email}
-              onChange={handleInputChange}
-              className="login-input"
-              name="email"
-              required
-            />
-            <br />
-            <label className="login-lable">Address</label>
-            <br />
-            <input
-              type="text"
-              value={user.address}
-              onChange={handleInputChange}
-              className="login-input"
-              name="address"
-              required
-            />
-            <br />
-            <label className="login-lable">Phone</label>
-            <br />
-            <input
-              name="phone"
-              value={user.phone}
-              onChange={handleInputChange}
-              type="tel"
-              pattern="[0-9]{10}"
-              maxLength={10}
-              title="Please enter a 10-digit phone number"
-              className="login-input"
-              required
-            />
-            <br />
-            <label className="login-lable">Password</label>
-            <br />
-            <input
-              name="password"
-              value={user.password}
-              onChange={handleInputChange}
-              type="password"
-              className="login-input"
-              required
-            />
-            <br />
-            <label className="login-lable">Confirm Password</label>
-            <br />
-            <input
-              type="password"
-              value={user.confirmPassword}
-              onChange={handleInputChange}
-              className="login-input"
-              name="confirmPassword"
-              required
-            />
-            <br />
+  //     <div className="user_tabl_towcolum">
+  //       <div className="left_colum_user">
+  //         <img src={regiimg} alt="regi img" className="regi_img" />
+  //       </div>
+  //       <div className="riight_colum_user">
+  //         <form className="regi-form" onSubmit={handleSubmit}>
+  //           <label className="login-lable">Full Name</label>
+  //           <br />
+  //           <input
+  //             type="text"
+  //             name="name"
+  //             value={user.name}
+  //             onChange={handleInputChange}
+  //             className="login-input"
+  //             required
+  //           />
+  //           <br />
+  //           <label className="login-lable">Email</label>
+  //           <br />
+  //           <input
+  //             type="email"
+  //             value={user.email}
+  //             onChange={handleInputChange}
+  //             className="login-input"
+  //             name="email"
+  //             required
+  //           />
+  //           <br />
+  //           <label className="login-lable">Address</label>
+  //           <br />
+  //           <input
+  //             type="text"
+  //             value={user.address}
+  //             onChange={handleInputChange}
+  //             className="login-input"
+  //             name="address"
+  //             required
+  //           />
+  //           <br />
+  //           <label className="login-lable">Phone</label>
+  //           <br />
+  //           <input
+  //             name="phone"
+  //             value={user.phone}
+  //             onChange={handleInputChange}
+  //             type="tel"
+  //             pattern="[0-9]{10}"
+  //             maxLength={10}
+  //             title="Please enter a 10-digit phone number"
+  //             className="login-input"
+  //             required
+  //           />
+  //           <br />
+  //           <label className="login-lable">Password</label>
+  //           <br />
+  //           <input
+  //             name="password"
+  //             value={user.password}
+  //             onChange={handleInputChange}
+  //             type="password"
+  //             className="login-input"
+  //             required
+  //           />
+  //           <br />
+  //           <label className="login-lable">Confirm Password</label>
+  //           <br />
+  //           <input
+  //             type="password"
+  //             value={user.confirmPassword}
+  //             onChange={handleInputChange}
+  //             className="login-input"
+  //             name="confirmPassword"
+  //             required
+  //           />
+  //           <br />
 
-            <button type="submit" className="admin_form_cneter_btn">
-              Register
-            </button>
-            <div>
-              <p className="no-acc">
-                Already have an account?{" "}
-                <NavLink to={`/login`}>
-                  <span className="no-acc-reg">Login</span>
-                </NavLink>
-              </p>
+  //           <button type="submit" className="admin_form_cneter_btn">
+  //             Register
+  //           </button>
+  //           <div>
+  //             <p className="no-acc">
+  //               Already have an account?{" "}
+  //               <NavLink to={`/login`}>
+  //                 <span className="no-acc-reg">Login</span>
+  //               </NavLink>
+  //             </p>
+  //           </div>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
+  <div className="flex items-center justify-center min-h-screen p-4">
+        <form className=" border rounded-md border-gray-300 p-4 w-full sm:w-auto " onSubmit={handleSubmit} encType='multipart/form-data'>
+        <div className="space-y-12">
+            <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-xl  font-sans font-semibold leading-7 text-gray-900">User Resgistration</h2>
+            <div className="lg:w-[600px] mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 w-auto">
+            
+            {/* product title */}
+            <div className="col-span-full">
+                <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+                    Full Name
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={user.name}
+                    onChange={handleInputChange}
+                    autoComplete="name"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
             </div>
-          </form>
+
+            {/* email */}
+                <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={user.email}
+                    onChange={handleInputChange}
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                {/* phone */}
+                <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Phone
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    value={user.phone}
+                    onChange={handleInputChange}
+                    pattern="[0-9]{10}"
+                    maxLength={10}
+                    title="Please enter a 10-digit phone number"
+                    autoComplete="phone"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                 {/* address */}
+                <div className="col-span-full">
+                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                    Address
+                </label>
+                <div className="mt-2">
+                    <textarea
+                    id="Address"
+                    name="Address"
+                    value={user.address}
+                    onChange={handleInputChange}
+                    rows={4}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+                
+                <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={user.password}
+                    onChange={handleInputChange}
+                    autoComplete="password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Confirm Password
+                </label>
+                <div className="mt-2">
+                    <input
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    value={user.confirmPassword}
+                    onChange={handleInputChange}
+                    autoComplete="confirmPassword"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+                </div>
+            
+            </div>
+            </div>
+        </div> 
+        <div>
+                <p className="text-center">
+                  Already have an account?{" "}
+                  <NavLink to={`/login`}>
+                    <span className="no-acc-reg">Login</span>
+                    </NavLink>
+                </p>
+              </div>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+            type="submit"
+            className="rounded-md bg-color4 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => navigate('/login')}
+            >
+            Cancel
+            </button>
+            <button
+            type="submit"
+            className="rounded-md bg-color4 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            
+            >
+            Register
+            </button>
         </div>
-      </div>
+        </form>
     </div>
-  );
+  )
 }
 
 export default Register;
