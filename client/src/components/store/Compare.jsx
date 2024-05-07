a83982e1af166d442270b932d2d36ecc33c5574a
Suchindu Malith <122116912+Suchindu@users.noreply.github.com>
2024-05-07 18:22:58 +0530

    Reorganized My Branch
    wrong commit fix

--- client/src/components/store/Compare.jsx
+++ client/src/components/store/Compare.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useState, useEffect, useContext } from 'react';
 import CompareProduct from './CompareProduct';
+import axios from 'axios';
+import { useCompare } from '../../context/CompareContext';
 
 const ParentComponent = () => {
+
+  const { compareList } = useCompare();
+
   return (
-    <div className="m-4 p-4 border-2 border-gray-300 rounded-lg">
+    <div className="m-4 p-4 border-2 border-gray-300 rounded-lg relative">
     <h1 className="text-center text-2xl font-bold mb-4">Product Comparison</h1>
     <div className="m-4 flex flex-col sm:flex-row">
       <div className="w-full md:w-1/4 mt-20 bg-white rounded-lg p-2 md:block hidden">
@@ -18,9 +24,9 @@ const ParentComponent = () => {
         </table>
       </div>
       <div className="w-full md:w-3/4 flex flex-col md:flex-row justify-between pl-4">
-        <CompareProduct />
+        {compareList.length > 0 && <CompareProduct id={compareList[0]}  />}
         <div className="hidden md:block border-l-2 border-gray-200"></div>
-        <CompareProduct />
+        {compareList.length > 1 && <CompareProduct id={compareList[1]}  />}
       </div>
     </div>
   </div>
