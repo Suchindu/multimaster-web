291ea503d3d2f6eff5341cb49a992038a572c38f
Suchindu Malith <122116912+Suchindu@users.noreply.github.com>
2024-05-09 03:33:27 +0530

    Product Overview Redesign

--- client/src/components/store/Product.jsx
+++ client/src/components/store/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import {addToCart} from '../../redux/cartSlice';
 import { useCompare } from '../../context/CompareContext';
   
